af3e4d0204452f98fd83bc583aafb314471f79fe
Jacson Curtis <jacson.curtis@ideagenplc.com>
2018-06-29 11:47:35 +0100

    Initial Commit

---
+++ DeceasedLatch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DeceasedLatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.Run();
+        }
+
+        public void Run()
+        {
+
+        }
+    }
+}
